f82061089012a9ebabc85521d30fe8f6649b6232
GOWREESH07 <gowreeshappathurai@gmail.com>
2026-02-09 21:32:07 +0530

    Add files via upload

---
+++ TWO SUM.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twosums(self,num,target):
+        n=len(num)
+        for i in range(n):
+            for j in range(i+1,n):
+                if num[i]+num[j] == target:
+                    return [i,j]
+        return []        
